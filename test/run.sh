bb8f3314a437ffececa9b6c2311b2c95296930df
Davide Principi <davide.principi@nethesis.it>
2019-11-04 17:57:30 +0100

    Fix wrapper script

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,9 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 if ! which bats &>/dev/null; then
-    yum --enablerepo=epel install bats
+    echo "The bats command is missing. You can install it with:"
+    echo "    yum --enablerepo=epel install bats"
+    exit 1
 fi
 
 bats bats/
