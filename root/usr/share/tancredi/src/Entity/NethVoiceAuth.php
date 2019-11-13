5a50465d7f7a2249c8051fafbb483327c4f7d9fb
Stefano Fancello <stefano.fancello@nethesis.it>
2019-11-13 16:13:27 +0100

    Configure NethVoice authentication on Tancredi

---
+++ root/usr/share/tancredi/src/Entity/NethVoiceAuth.php
@@ -0,0 +1,63 @@
+<?php namespace Tancredi\Entity;
+
+class NethVoiceAuth
+{
+    private $secret = NULL;
+    private $static_token = NULL;
+    private $dbh = NULL;
+
+    public function __construct($config = null) {
+        $config['secret'] ? $this->secret = $config['secret'] : null;
+        $config['static_token'] ? $this->static_token = $config['static_token'] : null;
+        $this->dbh = new \PDO(
+            $config['auth_nethvoice_dbengine'] . ':dbname=' . $config['auth_nethvoice_dbname'] .';host=' . $config['auth_nethvoice_dbhost'],
+            $config['auth_nethvoice_dbuser'],
+            $config['auth_nethvoice_dbpass']
+        );
+    }
+
+    public function __invoke($request, $response, $next)
+    {
+        if ($request->isOptions()) {
+            $response = $next($request, $response);
+        } elseif ($request->hasHeader('StaticToken') and !empty($this->static_token) and $request->getHeaderLine('StaticToken') === $this->static_token) {
+            // Local autentication for NethCTI success
+            $response = $next($request, $response);
+	} elseif ($request->hasHeader('Secretkey') and !empty($this->secret)) {
+	    $stmt = $this->dbh->prepare("SELECT * FROM ampusers WHERE sections LIKE '%*%' AND username = ?");
+	    $stmt->execute(array($request->getHeaderLine('User')));
+	    $user = $stmt->fetchAll();
+            $password_sha1 = $user[0]['password_sha1'];
+            $username = $user[0]['username'];
+
+            # check the user is valid and is an admin (sections = *)
+            if (!$username) {
+                return $response->withJson(['error' => 'Forbidden: invalid user'], 403);
+            }
+            $hash = sha1($username . $password_sha1 . $this->secret);
+            if ($request->getHeaderLine('Secretkey') != $hash) {
+                $results = array(
+                    'type' => 'https://github.com/nethesis/tancredi/wiki/problems#wrong-password',
+                    'title' => 'Wrong password'
+                );
+                $response = $response->withJson($results, 403);
+                $response = $response->withHeader('Content-Type', 'application/problem+json');
+                $response = $response->withHeader('Content-Language', 'en');
+            } else {
+                $response = $next($request, $response);
+            }
+        } else {
+            $results = array(
+                'type' => 'https://github.com/nethesis/tancredi/wiki/problems#missing-secret',
+                'title' => 'Missing secret key',
+                'Secretkey' => $request->getHeaderLine('Secretkey'),
+                'secret' => $request->getHeaderLine($this->secret)
+            );
+            $response = $response->withJson($results, 403);
+            $response = $response->withHeader('Content-Type', 'application/problem+json');
+            $response = $response->withHeader('Content-Language', 'en');
+        }
+        return $response;
+    }
+}
+
