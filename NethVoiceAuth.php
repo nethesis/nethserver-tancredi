e680ef20968f2e08bbff70aa46f5df539cbe3a93
Stefano Fancello <stefano.fancello@nethesis.it>
2019-11-15 12:13:19 +0100

    NethVoiceAuth: better naming and enancements

--- NethVoiceAuth.php
+++ NethVoiceAuth.php
@@ -2,32 +2,29 @@
 
 class NethVoiceAuth
 {
-    private $secret;
-    private $static_token;
-    private $dbh;
+    private $config;
 
     public function __construct($config) {
         if (!is_array($config) or !array_key_exists('secret',$config) or !array_key_exists('static_token',$config)) {
             throw new RuntimeException('Wrong or missing configuration');
         }
-        $this->secret = $config['secret'];
-        $this->static_token = $config['static_token'];
-        $this->dbh = new \PDO(
-            'mysql:dbname=asterisk;host=localhost',
-            'tancredi',
-            $config['auth_nethvoice_dbpass']
-        );
+        $this->config = $config;
     }
 
     public function __invoke($request, $response, $next)
     {
         if ($request->isOptions()) {
             $response = $next($request, $response);
-        } elseif ($request->hasHeader('StaticToken') and !empty($this->static_token) and $request->getHeaderLine('StaticToken') === $this->static_token) {
+        } elseif ($request->hasHeader('Authentication:Static') and !empty($this->config['static_token']) and ($request->getHeaderLine('HTTP_HOST') === '127.0.0.1' or $request->getHeaderLine('HTTP_HOST') === 'localhost') and $request->getHeaderLine('Authentication:Static') === $this->config['static_token']) {
             // Local autentication for NethCTI success
             $response = $next($request, $response);
-	} elseif ($request->hasHeader('Secretkey') and !empty($this->secret)) {
-            $stmt = $this->dbh->prepare("SELECT * FROM ampusers WHERE sections LIKE '%*%' AND username = ?");
+	} elseif ($request->hasHeader('Secretkey') and !empty($this->config['secret'])) {
+            $dbh = new \PDO(
+                'mysql:dbname=asterisk;host=localhost',
+                'tancredi',
+                $this->config['auth_nethvoice_dbpass']
+            );
+            $stmt = $dbh->prepare("SELECT * FROM ampusers WHERE sections LIKE '%*%' AND username = ?");
             $stmt->execute(array($request->getHeaderLine('User')));
             $user = $stmt->fetchAll();
             $password_sha1 = $user[0]['password_sha1'];
@@ -37,11 +34,11 @@ class NethVoiceAuth
             if (!$username) {
                 return $response->withJson(['error' => 'Forbidden: invalid user'], 403);
             }
-            $hash = sha1($username . $password_sha1 . $this->secret);
+            $hash = sha1($username . $password_sha1 . $this->config['secret']);
             if ($request->getHeaderLine('Secretkey') != $hash) {
                 $results = array(
-                    'type' => 'https://github.com/nethesis/tancredi/wiki/problems#wrong-password',
-                    'title' => 'Wrong password'
+                    'type' => 'https://nethesis.github.io/tancredi/problems#forbidden',
+                    'title' => 'Access to resource is forbidden with current client privileges'
                 );
                 $response = $response->withJson($results, 403);
                 $response = $response->withHeader('Content-Type', 'application/problem+json');
@@ -51,10 +48,10 @@ class NethVoiceAuth
             }
         } else {
             $results = array(
-                'type' => 'https://github.com/nethesis/tancredi/wiki/problems#missing-secret',
-                'title' => 'Missing secret key',
+                'type' => 'https://nethesis.github.io/tancredi/problems#forbidden',
+                'title' => 'Forbidden',
                 'Secretkey' => $request->getHeaderLine('Secretkey'),
-                'secret' => $request->getHeaderLine($this->secret)
+                'secret' => $request->getHeaderLine($this->config['secret'])
             );
             $response = $response->withJson($results, 403);
             $response = $response->withHeader('Content-Type', 'application/problem+json');
