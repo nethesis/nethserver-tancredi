8446c5d7a2551bf548ed49335116d26cf0f03dbe
Davide Principi <davide.principi@nethesis.it>
2020-03-04 10:16:14 +0100

    Always set our runtime vars (#16)
    * Always set our runtime vars
    
    account_call_waiting, account_timeout_fwd_target, 
    account_busy_fwd_target,
    account_always_fwd_target,
    account_cftimeout,
    account_dnd_enable are always pushed among other variables at least as 
    empty strings.
    
    Removed some "_enable" variables: it is possible to just test the 
    corresponding "_target" variable instead.
    
    No longer depend on a "mainextension" variable. Calculate the main 
    extension value with NethVoice business rules.
    
    * Do not repeatly log scope contents
    
    * Extension length can be 2, 3 or 4
    
    Co-Authored-By: Stefano <stefano.fancello@nethesis.it>
    
    * Open/Close sqlite DB in same context
    
    Co-authored-by: Stefano <stefano.fancello@nethesis.it>

--- AsteriskRuntimeFilter.php
+++ AsteriskRuntimeFilter.php
@@ -15,76 +15,75 @@
 */
 class AsteriskRuntimeFilter
 {
-    private $db;
     private $logger;
+    private $config;
 
     public function __construct($config,$logger)
     {
-        $this->db = new \SQLite3($config['astdb'],SQLITE3_OPEN_READONLY);
+        $this->config = $config;
         $this->logger = $logger;
     }
 
     public function __invoke($variables)
     {
-        // Get account index
-        $indexes = array();
-        foreach (array_keys($variables) as $key) {
-            if (strpos('account_username_', $key)) {
-                $indexes[] = str_replace('account_username_','',$key);
-            }
-        }
-        if (empty($indexes)) $indexes[] = 1;
-
-        foreach ($indexes as $index) {
-            if (!empty($variables['mainextension_'.$index])) {
-                $extension = $variables['mainextension_'.$index];
-            } elseif (!empty($variables['extension_'.$index])) {
-                $extension = $variables['extension_'.$index];
-            } elseif (!empty($variables['account_username_'.$index])) {
-                $extension = $variables['account_username_'.$index];
-            } else {
-                return $variables;
+        $db = new \SQLite3($this->config['astdb'],SQLITE3_OPEN_READONLY);
+        foreach (array_keys($variables) as $variable) {
+            if(substr($variable, 0, 18) != 'account_extension_') {
+                // Ignore all variables except those starting with "account_extension_"
+                continue;
             }
 
-            $statement = $this->db->prepare('SELECT key,value FROM astdb WHERE key LIKE :key');
+            $index = (integer) substr($variable, 18);
+            if($index <= 0) {
+                // index must be a positive integer
+                continue;
+            }
+
+            // Initialize default values
+            $variables['account_call_waiting_' . $index] = '';
+            $variables['account_timeout_fwd_target_' . $index] = '';
+            $variables['account_busy_fwd_target_' . $index] = '';
+            $variables['account_always_fwd_target_' . $index] = '';
+            $variables['account_cftimeout_' . $index] = '';
+            $variables['account_dnd_enable_' . $index] = '';
+
+            $mainextension_match = array();
+            $extension = $variables[$variable];
+            if(preg_match("/^9\d(\d{2,4})$/", $extension, $mainextension_match)) {
+                $extension = $mainextension_match[1];
+            }
+
+            $statement = $db->prepare('SELECT key,value FROM astdb WHERE key LIKE :key');
             $statement->bindValue(':key', "%/$extension%");
-
             $results = $statement->execute();
 
-            $variables['call_waiting'] = 0;
-            $variables['dnd_enable'] = 0;
             while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
                 if ($row['key'] == "/CW/$extension" && $row['value'] == 'ENABLED') {
-                    $variables['call_waiting_'.$index] = 1;
-                }
+                    $variables['account_call_waiting_' . $index] = '1';
+                } 
 
                 if ($row['key'] == "/CFU/$extension") {
-                    $variables['timeout_fwd_target_'.$index] = $row['value'];
-                    $variables['timeout_fwd_enable_'.$index] = (int) !empty($row['value']);
+                    $variables['account_timeout_fwd_target_' . $index] = $row['value'];
                 }
 
                 if ($row['key'] == "/CFB/$extension") {
-                    $variables['busy_fwd_target_'.$index] = $row['value'];
-                    $variables['busy_fwd_enable_'.$index] = (int) !empty($row['value']);
+                    $variables['account_busy_fwd_target_' . $index] = $row['value'];
                 }
 
                 if ($row['key'] == "/CF/$extension") {
-                    $variables['always_fwd_target_'.$index] = $row['value'];
-                    $variables['always_fwd_enable_'.$index] = (int) !empty($row['value']);
+                    $variables['account_always_fwd_target_' . $index] = $row['value'];
                 }
 
                 if ($row['key'] == "/AMPUSER/$extension/followme/prering") {
-                    $variables['cftimeout_'.$index] = $row['value'];
+                    $variables['account_cftimeout_' . $index] = $row['value'];
                 }
 
                 if ($row['key'] == "/DND/$extension" && $row['value'] == 'YES') {
-                    $variables['dnd_enable_'.$index] = 1;
+                    $variables['account_dnd_enable_' . $index] = '1';
                 }
             }
         }
-        $this->logger->debug(__CLASS__ . "Added runtime variables");
-        $this->db->close();
+        $db->close();
         return $variables;
     }
 }
-
